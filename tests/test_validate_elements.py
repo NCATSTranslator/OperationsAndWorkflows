c0c5cfd9b9e5f3ce4296a73d19358d1a8cd82eba
cbizon <bizon@renci.org>
2023-05-25 14:24:11 -0400

    cleaning tests

--- tests/test_validate_elements.py
+++ tests/test_validate_elements.py
@@ -1,8 +1,10 @@
 from jsonschema import validate, ValidationError
+import os
 import json
 
 def test_validate_allowlist():
-    with open("../schema/operation.json","r") as f:
+    opjson = os.path.join(os.path.dirname(__file__), '../schema/operation.json')
+    with open(opjson,"r") as f:
         schema=json.load(f)
     validate(instance={"id": "lookup",'runner_parameters': { "allowlist": ["x"] } }, schema=schema)
     valid = False
